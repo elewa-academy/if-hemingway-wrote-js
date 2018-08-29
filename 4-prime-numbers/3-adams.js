486c235f748dd77250af1b044e6662ed14254701
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2018-08-29 23:55:52 +0200

    fix

--- 4-prime-numbers/3-adams.js
+++ 4-prime-numbers/3-adams.js
@@ -5,13 +5,13 @@ function kevinTheNumberMentioner(_){
 	l=[]
 	/* mostly harmless --> */ with(l) {
 
-	//Sorry about all this, my babel fish has a headache today...
-	for (ll=!+[]+!![];ll<_+(+!![]);ll++) {
-		lll=+!![];
-		while (ll%++lll);
+		//Sorry about all this, my babel fish has a headache today...
+		for (ll=!+[]+!![];ll<_+(+!![]);ll++) {
+			lll=+!![];
+			while (ll%++lll);
 			//I've got this terrible pain in all the semicolons down my right-hand side. 
 			(ll==lll)&&push(ll);
-		} 
+		}
 		forEach(alert);
 
 	}
